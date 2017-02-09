3948affaea0aeda205d831ee73b947c85d34fd7f
WittyParrot <WittyParrot>
2017-02-09 19:42:33 +0530

    commit sever.js file

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ var app = express()
 var port = process.env.PORT || 3000; 
 
 //console.log (__dirname);	
-		
-	app.get('/', function (req, res) {
+	app.use(express.static(path.join(__dirname, 'public')));
+	
+	app.get('*', function (req, res) {
 		//res.send('hello world')
-   res.sendFile(path.join(__dirname + '/public/index.html'));
+   res.sendFile(path.join(__dirname + '/public'));
 })
 
 //app.use(express.static(path.join(__dirname+ '/public')));
 //console.log(__dirname);
 //app.use('/public',express.static(path.join(__dirname, 'public/')));
-app.use(express.static(path.join(__dirname, 'public')));
    http.createServer(function (request, response) {
 
    // Send the HTTP header 
