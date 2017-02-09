909841f5eca1ffa7904dffa93912834741318122
WittyParrot <WittyParrot>
2017-02-09 18:55:46 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 3000;
 		
 	app.get('/', function (req, res) {
 		//res.send('hello world')
-   res.sendFile(path.join(__dirname + '/public/index.html'));
+   res.sendFile(path.join(__dirname + '/public'));
 })
 
 //app.use(express.static(path.join(__dirname+ '/public')));
